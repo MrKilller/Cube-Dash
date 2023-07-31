15e07d2faf4f82b21e6cac80670a8cc3c9d0ac18
Abhijeet Chowdhury <88799258+MrKilller@users.noreply.github.com>
2023-08-01 03:28:33 +0530

    Add files via upload

---
+++ Assets/Scripts/credits.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class credits : MonoBehaviour {
+
+	// Use this for initialization
+	public void Quit () {
+        Application.Quit();
+	}
+	
+	
+	
+}
